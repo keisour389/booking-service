ff9772fc3069ddcb3df00585f818eecc3fe8cc90
Tuan Kiet <limit389@gmail.com>
2025-06-02 22:35:42 +0700

    Init: setup-project Summary: - Setup project

---
+++ src/main/java/com/smart_physio_therapy/booking_service/BookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smart_physio_therapy.booking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingServiceApplication.class, args);
+	}
+
+}
